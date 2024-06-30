35701a2a9e90c79b402b26430d8e942d1f925fc0
Anastasiya Semenchenko <Anastas.fyrk@yandex.ru>
2024-06-30 19:51:18 +0300

    пр8 в1

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+//карточка товара
+export interface ICard {
+    _id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number;
+}
+
+//список карточек на странице
+export interface ICardList {
+    cards: ICard[];
+    preview: string | null;
+}
+
+//карточка в корзине
+export type TBasket = Pick<ICard,  'title' | 'price'>;
+
+//Форма способа оплаты и адреса
+export interface IFormOrder {
+    address: string;
+    payment: string;
+}
+
+//Форма контактных данных пользователя
+export interface IFormUser {
+    email: string;
+    phone: string;
+}
+
+//Проверка валидации формы оплаты и адреса
+export interface IFormOrderValidate {
+    checkValidation(data: Record<keyof IFormOrder, string>): boolean;
+}
+
+//Проверка валидации формы контактных данных
+export interface IFormUserValidate {
+    checkValidation(data: Record<keyof IFormUser, string>): boolean;
+}
